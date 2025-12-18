d70f2b99a822625cda3435ed2879d3282db2583c
pto_rojas <pto_rojas@hotmail.com>
2025-12-18 14:13:41 -0500

    Initial commit

---
+++ cliente_forma.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms.fields.numeric import IntegerField
+from wtforms.fields.simple import StringField, SubmitField, HiddenField
+from wtforms.validators import DataRequired
+
+
+class ClienteForma(FlaskForm):
+    id = HiddenField('id')
+    nombre =StringField('Nombre',validators=[DataRequired()])
+    apellido = StringField('Apellido',validators=[DataRequired()])
+    membresia = IntegerField('Membresia',validators=[DataRequired()])
+    guardar = SubmitField('Guardar')
